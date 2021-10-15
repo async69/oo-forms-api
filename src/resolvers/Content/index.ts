c3517a4ef2bbeada3638960c472cf92b238ad81e
Yeabsera <64396141+async69@users.noreply.github.com>
2021-10-15 23:42:24 +0300

    Update index.ts

--- src/resolvers/Content/index.ts
+++ src/resolvers/Content/index.ts
@@ -19,7 +19,7 @@ async function processLineByLine() {
   let count = 0;
   const data = [];
   for await (const line of rl) {
-    if (count === 100000) return data;
+    if (count === 1000000) return data;
     data.push(line)
     count++
   }
